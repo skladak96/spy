ffb6914f12218d9134f0374c4874a25e60e5e0d4
skladak96 <108767727+skladak96@users.noreply.github.com>
2022-07-07 07:41:41 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+///Edit Under This only
+
+  
+    var firebaseConfig = {
+    apiKey: "AIzaSyAOUBXwM1szuI_qO8XQBMbpUj0yNYHh0PA",
+authDomain: "skladak96-f6266.firebaseapp.com",
+databaseURL: "https://skladak96-f6266-default-rtdb.firebaseio.com",
+projectId: "skladak96-f6266",
+storageBucket: "skladak96-f6266.appspot.com",
+messagingSenderId: "791597737520",
+appId: "1:791597737520:web:0ba21b7e094a59c2e6eff5"
+}; 
+ 
+////Do Not Edit Below This
+
+firebase.initializeApp(firebaseConfig);
